a1b0f82a0b41abf00d6bdeb4f62857c3edfb61b1
Pierre Tasci <ptasci67@gmail.com>
2017-02-02 09:08:39 -0800

    Added email to the login

--- services/authentication.js
+++ services/authentication.js
@@ -20,11 +20,22 @@ module.exports = function(app) {
   passport.use(new FacebookStrategy({
     clientID: fb.app_id,
     clientSecret: fb.app_secret,
-    callbackURL: '/auth/facebook/callback'
+    callbackURL: '/auth/facebook/callback',
+    profileFields: ['email', 'name'],
   }, (accessToken, refreshToken, profile, done) => {
-    User.findOrCreate({
-      name: profile.displayName,
-    }, (err, user) => {
+    console.log(profile);
+    const u = {
+      name: `${profile.name.givenName} ${profile.name.familyName}`,
+    };
+
+    // Add the user's email.
+    if (!Array.isArray(profile.emails) || profile.emails.length === 0) {
+      console.error('Could not create user without email.');
+      return done("Could not create user without email.");
+    }
+    u.email = profile.emails[0].value;
+    console.log(u);
+    User.findOrCreate(u, (err, user) => {
       if (err) {
         console.error(err);
         return done(err);
--- services/sessions.js
+++ services/sessions.js
@@ -1,12 +1,15 @@
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 
+const SECONDS_P_HOUR = 3600;
+
 module.exports = function(app) {
   config = app.locals.config;
   app.use(session({
     store: new RedisStore({
       host: config.get('host'),
       port: 6379,
+      ttl: SECONDS_P_HOUR * 24 * 365, // 1 year.
     }),
     secret: config.get('secret'),
     resave: true,
