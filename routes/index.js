3ee1eeaec58442591105a7376cb8680b35f38e09
Pierre Tasci <ptasci67@gmail.com>
2017-02-02 08:55:15 -0800

    Simple login and logout pages added

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'PenPal' });
+router.get('/', (req, res, next) => {
+  if (!req.user) {
+    return res.redirect('/login');
+  }
+  res.render('index', { title: 'PenPal', username: req.user.name });
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('login', { title: 'PenPal - Login' });
+})
+
+router.get('/logout', (req, res, next) => {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
